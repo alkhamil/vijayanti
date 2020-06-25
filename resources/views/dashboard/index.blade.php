e4ab856a35bd1cc5396028d6e0f6ec7c994f226c
viraka <virakaprilitya76@gmail.com>
2020-06-25 17:08:00 +0700

    dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,8 +9,138 @@
 <div class="row">
     <div class="col-md-12">
         <div class="tile">
-            <div class="tile-body">Dashboard</div>
+            <div class="tile-body">
+                <div class="">
+                    <select id="select" class="form-control" >
+                            <option value="" selected>-</option>
+                        @foreach ($assignments as $key=>$item)
+                            <option value="{{$item->company->id}}">{{$item->company->name}}</option>
+                        @endforeach
+                    </select>
+                </div>
+                <br>
+                <div id="dt1">
+                    <span class="d-block p-2 bg-primary text-white">Perhitungan semua berdasarkan pernyataan</span>
+                    <canvas id="myChart" width="400" height=200"></canvas>
+                </div>
+                <div id="dt2">
+                    <span class="d-block p-2 bg-primary text-white">Nilai Rata - Rata Gap 5 berdasarkan dimensi</span>
+                    <canvas id="myDimensi" width="400" height=200"></canvas>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script>
+    $('#dt1').hide();
+    $('#dt2').hide();
+
+    $('#select').change(() => {
+        var data = $('#select').val();
+        datas(data);
+        dimensi(data);
+        
+        $('#dt1').show();
+        $('#dt2').show();
+    })
+
+    function datas(data){
+        var id = data;
+        var urls = "{{url('')}}";
+        var url =urls + '/dashboard/chart/' + id;
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var Labels = new Array();
+        var Labels2 = new Array();
+        var Datas = new Array();
+        var Datas2 = new Array();
+        var Ket = new Array();
+        var color = new Array();
+        var color2 = new Array();
+
+        $.get(url, function(response){
+        // console.log(response.)
+            response.forEach(function(data){
+                Labels.push(data.id)
+                Datas.push(data.nilai)
+                color.push('rgba(' + colorGen () +', ' + colorGen () +',' + colorGen () +' )')
+            });
+            var myChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: Labels,
+                    datasets: [{
+                        label: [],
+                        data: Datas,
+                        backgroundColor: color,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+
+        
+        });
+    }
+
+    function dimensi(data){
+
+        var id = data;
+        var urls = "{{url('')}}";
+        var url2 =urls + '/dashboard/chartDimensi/' + id;
+        var ctx2 = document.getElementById('myDimensi').getContext('2d');
+        var Labels2 = new Array();
+        var Datas2 = new Array();
+        var Ket = new Array();
+        var color2 = new Array();
+
+        $.get(url2, function(response){
+        // console.log(response);
+        response.forEach(function(data){
+            Labels2.push(data.name)
+            Datas2.push(data.nilai)
+            color2.push('rgba(' + colorGen () +', ' + colorGen () +',' + colorGen () +' )')
+        });
+        var myChart = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: Labels2,
+                datasets: [{
+                    label: [],
+                    data: Datas2,
+                    backgroundColor: color2,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+    });
+       
+    }
+    
+
+    function colorGen () { 
+        var generateColor = Math.floor(Math.random() * 256 );
+        return generateColor;
+    }
+   
+</script>
 @endsection
